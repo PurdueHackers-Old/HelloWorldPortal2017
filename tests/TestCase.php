834d443081575efc5d745fd2f86133e41765b585
Jacob Brabec <jbrabec@purdue.edu>
2017-07-28 22:40:17 -0700

    Prevent accidental sending of emails during tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,8 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    private function prepareForTests() {
+     //Log email instead of sending, regardless of config
+     Mail::pretend(true);
+   }
 }
