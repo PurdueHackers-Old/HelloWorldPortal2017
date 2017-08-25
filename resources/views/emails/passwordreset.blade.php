74a347c45230073a19f450c858230801ff136f46
Jake Brabec <jbrabec@purdue.edu>
2017-08-25 17:37:03 -0400

    Modified email templates to help prevent cisco from breaking them

--- resources/views/emails/passwordreset.blade.php
+++ resources/views/emails/passwordreset.blade.php
@@ -1,1 +1,2 @@
 Please click <a href="{{$url}}">here</a> to reset your password for Hello World.<br><br>
+If the above link doesn't work for you, please try copy/pasting the following link directly into the browser: {{$url}}
