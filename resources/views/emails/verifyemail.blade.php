5ffbd5ffea044de955f7dd6da64a94230f59b357
Jake Brabec <jbrabec@purdue.edu>
2017-09-04 15:40:21 -0400

    Link to token directly in emails

--- resources/views/emails/verifyemail.blade.php
+++ resources/views/emails/verifyemail.blade.php
@@ -1,2 +1,2 @@
 Please click <a href="{{$url}}">here</a> to confirm your email for Hello World.<br><br>
-If the above link doesn't work for you, please try copy/pasting the following link directly into the browser: {{$url}}
+If the above link doesn't work for you, please enter the following code into the verification page manually: {{$token}}
