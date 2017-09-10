eb8c322d908df010c1a13d5cea1921041b53f8df
Jake Brabec <jbrabec@purdue.edu>
2017-09-10 13:28:47 -0400

    Added resume processing artisan commands

---
+++ app/Console/Commands/DownloadResumes.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use File;
+
+class DownloadResumes extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'resumes:download';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Download files from S3 into ./storage/app/copied_resumes
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+      $this->info('Downloading resumes...');
+      $bucketName = getenv('AWS_BUCKET');
+      $targetPath = "./storage/app/copied_resumes";
+
+      if(!$bucketName) {
+        $this->info('Undefined bucket name! Set AWS_BUCKET env variable');
+        return;
+      }
+      $downloadCommand = "aws s3 cp --recursive s3://".$bucketName." ".$targetPath;
+
+
+      $this->info("Copying resumes from AWS bucket: ".$bucketName);
+      $this->info("Copying resumes into RELATIVE path (check working dir!): ".$targetPath);
+      $this->info("Command to Execute:\n".$downloadCommand);
+
+      if(!$this->confirm("Are you sure?")) {
+        $this->info('Aborting...');
+        return;
+      }
+
+      //Show warning if files already exist
+      if(File::exists($targetPath)) {
+        // This folder already exists
+        if(!$this->confirm("A folder already exists on the target path! It will be overwritten.\nDo you want to continue anyways?")) {
+          $this->info('Aborting...');
+          return;
+        }
+      }
+
+      $process = new Process($downloadCommand);
+      $process->run();
+      if (!$process->isSuccessful()) {
+        throw new ProcessFailedException($process);
+      } else {
+        $this->info("Output:".$process->getOutput());
+        $this->info("Success");
+      }
+    }
+}
---
+++ app/Console/Commands/ProcessResumes.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+use App\Models\Checkin;
+use DB;
+use \Symfony\Component\Process\Process;
+
+use File;
+use Storage;
+
+class ProcessResumes extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'resumes:process';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+        ];
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+      $this->info('Processing resumes...');
+      $sourcePath = "./storage/app/copied_resumes/resumes/";
+      $destPath = "./storage/app/cleaned_resumes/resumes/";
+      $storagePathInput = "copied_resumes/resumes/";
+      $storagePathOutput = "cleaned_resumes/resumes/";
+
+      //Show warning if input files do not already exist
+      if(!File::exists($sourcePath)) {
+        // This folder doesnt exist
+        $this->info("The ".$sourcePath." folder does not exist! Aborting...");
+        $this->info('Aborting...');
+        return;
+      }
+      //Show warning if output files  already exist
+      if(File::exists($destPath)) {
+        // This folder already exists
+        $this->info("The ".$destPath." folder already exists! Aborting...");
+        $this->info('Aborting...');
+        return;
+      }
+
+      $checkedInUsers = DB::table('users')
+        ->join('checkins','users.id','checkins.user_id')
+        ->join('resumes','users.id','resumes.user_id')
+        ->select('users.*','checkins.*','resumes.*')
+        ->get();
+
+      foreach ($checkedInUsers as $user) {
+        $resume_name = "resume_".$user->uuid.".pdf";
+        $new_name = "resume_".$user->firstname.".".$user->lastname.".pdf";
+        $this->info("User ".$user->email." resume: ".$resume_name);
+        Storage::disk('local')->move($storagePathInput.$resume_name, $storagePathOutput.$new_name);
+      }
+    }
+}
